6c63a89458a271a9622a805dcd7d507776f23a77
endepointe <endepointe@gmail.com>
2023-11-12 14:40:16 -0800

    check config file and read entries

--- src/interface.rs
+++ src/interface.rs
@@ -1,20 +1,106 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use clap::Parser;
 
+// https://doc.rust-lang.org/stable/book/ch19-06-macros.html
+macro_rules! show_field_names {
+    (struct $name:ident { $($fname:ident : $ftype:ty),* }) => {
+        struct $name {
+            $($fname : $ftype),*
+        }
+
+        impl $name {
+            fn field_names() -> &'static [&'static str] {
+                static NAMES: &'static [&'static str] = &[$(stringify!($fname)),*];
+                NAMES
+            }
+        }
+    }
+}
+
+show_field_names!{
+struct Config {
+    server_address: String,
+    server_port: u16,
+    log_file_path: String,
+    field_values: Vec<String>,
+    credentials: String // TLS needed
+}}
+
+
+pub fn
+read_config() -> Result<u8, Box<dyn std::error::Error>> {
+    let file = File::open("test.config")?;
+    let reader = BufReader::new(file);
+    let mut result;
+    for line in reader.lines() {
+        result = line?.clone();
+        let res = &check_config_file(result.split(" ").collect::<Vec<_>>()[0]);
+        match res {
+            Some(val) => println!("{:?}",val),
+            None => println!("not a line with data")
+        }
+    }
+    /*
+     * TODO read the config entries and establish connection with server*/
+    Ok(0)
+}
+
+/*
+ * Check the configuration file for proper format
+ * */
+fn
+check_config_file(entry: &str) -> Option<&str> {
+    if entry != "#".to_string() {
+        if Config::field_names().contains(&entry) == true && &entry.len() > &0 {
+            //println!("{:?} -- {:?} -- {}", Config::field_names().contains(&entry), &entry, &entry.len());
+            return Some(&entry);
+        }
+    }
+    None
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// UNUSED Section
+/*
 #[derive(Parser,Debug)]
 struct Args {
     config: String,
 }
-
-#[derive(Debug)]
-pub struct Config {
-    server_address: String,
-    server_port: u16,
-    log_file_path: String,
-    credentials: String, // TLS needed
-}
-
 // the user could provide a path to a different config or simply get help with using the command
 pub fn 
 command_line() -> Result<Config, Box<dyn std::error::Error>>{
@@ -24,16 +110,9 @@ command_line() -> Result<Config, Box<dyn std::error::Error>>{
         server_address: String::from("server address"),
         server_port: 123,
         log_file_path: String::from("path to log file"),
+        field_values: vec!["test".to_string(), "field".to_string(), "values".to_string()],
         credentials: String::from("credentials"),
     })
 }
+*/
 
-pub fn
-read_config() -> Result<u8, Box<dyn std::error::Error>> {
-    let file = File::open("test.config")?;
-    let reader = BufReader::new(file);
-    for line in reader.lines() {
-        println!("{}", line?);
-    }
-    Ok(0)
-}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::env;
 mod producer;
-use crate::producer::{run_tail_f};
+//use crate::producer::{run_tail_f};
 
 mod interface;
-use interface::{command_line,read_config};
+use interface::{read_config};
 
 fn main() {
     match env::args().len() {
-        1 => {read_config();}
+        1 => {let _ = read_config();}
         _ => {println!("handle command line usage");}
     }
 }
