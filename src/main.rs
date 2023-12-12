36e959a7e56fdb23c34b37bbb3a87d908cc8154c
endepointe <endepointe@gmail.com>
2023-12-11 22:31:55 -0800

    client reading and sending logs

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use std::io::{Read, Write};
 
 fn main() {
     let mut config_data;
-    //
     match env::args().len() {
         1 => {
             config_data = read_config();
@@ -17,16 +16,7 @@ fn main() {
                 config_data.unwrap()[0].to_string()) {
                 eprintln!("Error: {}", err);
             }
-            //let tail_output = run_tail_f(config_data.unwrap()[0].to_string());
-            //println!("{}", String::from(&tail_output));
         }
         _ => {println!("handle command line usage");}
     }
-    //
-
-    /*
-    let mut stream = TcpStream::connect("127.0.0.1:44331").expect("failed to connect to server");
-    let message = "Hello, server!";
-    stream.write_all(message.as_bytes()).expect("Failed to send data to server");
-    */
 }
