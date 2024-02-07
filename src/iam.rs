7d5234aecd483f1a5a998e04a8c5dd633def622e
Ivan <jicxer@gmail.com>
2024-02-07 00:26:54 -0800

    Worked further on IAM implementation. Added comments to brainstorm solutions and thinking process.
    Modifications
    
    src/iam.rs:
    	Implemented the delete user and list user. Functions now work.
    src/util.rs:
    	Implemented a scenario where a testuser is created, list all the
    current users in the IAM, delete the user, check the list.

--- src/iam.rs
+++ src/iam.rs
@@ -6,11 +6,27 @@ use aws_sdk_iam::operation::{
     list_roles::*, list_saml_providers::*, list_users::*,
 };
 use aws_sdk_iam::error::SdkError;
-
+use tokio::time::{sleep, Duration};
 use aws_sdk_iam::types::{AccessKey, Policy, PolicyScopeType, Role, User};
 
 
 // #[::tokio::main]
+
+
+/*
+    Goals:
+    -[] Assign minimal permission policies based on need
+    -[] Assign minimal permission policies based on all
+    -[] List policies 
+    -[] Detach policies
+        Maybe the logistics can be better managed on the AWS rather than CMND line. 
+        Should we still offer the option?
+    -[] Create a specifc customer role on AWS IAM
+        - contains minimal permissions
+    -[] 
+
+
+]*/
 pub async fn
 start_iam() -> Result<Client, Error> {
     let config = aws_config::load_from_env().await;
@@ -18,7 +34,16 @@ start_iam() -> Result<Client, Error> {
     Ok(client)
 }
 
+pub async fn create_user(client: &Client, user_name: &str) -> Result<User, Error> {
+    let response = client.create_user().user_name(user_name).send().await?;
 
+    Ok(response.user.unwrap())
+}
+
+
+/*
+    Requires IAM policy ListUsers
+ */
 pub async fn list_users(
     client: &Client,
     path_prefix: Option<String>,
@@ -33,4 +58,32 @@ pub async fn list_users(
         .send()
         .await?;
     Ok(response)
+}
+
+pub async fn delete_user(client: &Client, user: &User) -> Result<(), SdkError<DeleteUserError>> {
+    let user = user.clone();
+    let mut tries: i32 = 0;
+    let max_tries: i32 = 10;
+
+    let response: Result<(), SdkError<DeleteUserError>> = loop {
+        match client
+            .delete_user()
+            .user_name(user.user_name())
+            .send()
+            .await
+        {
+            Ok(_) => {
+                break Ok(());
+            }
+            Err(e) => {
+                tries += 1;
+                if tries > max_tries {
+                    break Err(e);
+                }
+                sleep(Duration::from_secs(2)).await;
+            }
+        }
+    };
+
+    response
 }
--- src/util.rs
+++ src/util.rs
@@ -6,6 +6,7 @@ use crate::producer::start_log_stream;
 use crate::dynamosdk; // Import other modules as needed
 use std::{env, process};
 use crate::iam;
+use aws_sdk_iam::types::User;
 
 pub async fn send_logs_to_all_destinations(config: Config) {
     // Call the functions to send logs to all destinations
@@ -26,28 +27,81 @@ pub async fn print_help() {
     process::exit(0);
 }
 
+
+/*
+    What I'm thinking the purpose of this function could be is to create a new user
+    Assign a specific pre-created role that specifies customers with limited permissions such Read only
+        These would require all the policies needed for using S3, DynamoDB, Firehose, etc..
+        This could be an option where the user requires send to "all"
+
+    Another approach would be if we don't want a user to gain permissions to resources they are not using is to
+    assign each policy to use those resources individually.
+
+    Regardless, an option would be to:
+    1. Create a user for a customer
+    2. Assign roles to user based on needs (or all)
+    3. User uses resources
+    4. Delete the user?
+
+    We could also follow the "Apply least-privilege permissions" scenario listed on AWS:
+        "When you set permissions with IAM policies, grant only the permissions required to perform a task. 
+        You do this by defining the actions that can be taken on specific resources under specific conditions, 
+            also known as least-privilege permissions. 
+        You might start with broad permissions while you explore the permissions that are required for your workload or use case. 
+        As your use case matures, you can work to reduce the permissions that you grant to work toward least privilege. 
+        For more information about using IAM to apply permissions"
+
+    In this case, users will simply receive the policies based on the resouces they need.
+*/
 pub async fn initialize_iam(config:Config){
 
     let iam_client = iam::start_iam().await;
     println!("{:?}",&config);
-
-    match iam_client{
+    match iam_client {
         Ok(client) => {
-            if let Ok(users) = iam::list_users(&client,
-                None,
-                None,
-                None).await {
-                    print!("Printing users: ");
-                    for user in users.users {
-                        println!("{}", user.user_name);
-                }
             
+            // Create a test user named "testuser"
+            let testuser = "testuser";
+            println!("\nStarted the IAM client\nCreating user: testuser");
+            let created_user = iam::create_user(&client, testuser).await.unwrap();
+
+            // List all the current users; must require IAM policy
+            println!("Listing all current users along with added testuser");
+            let users = iam::list_users(&client, None, None, None)
+            .await
+            .unwrap();
+            for user in users.users {
+                println!("{}", user.user_name);
+            }
+
+            // Delete the test user
+            println!("\nDeleting user: testuser");
+            match iam::delete_user(&client, &created_user).await {
+                Ok(_) => {
+                    println!("\nUser 'testuser' deleted successfully\n");
+                }
+                Err(err) => {
+                    eprintln!("Error deleting user 'testuser': {:?}", err);
+                    // Handle the error as needed
+                }
+            }
+            println!("Current users after testuser deletion:");
+            let users = iam::list_users(&client, None, None, None)
+            .await
+            .unwrap();
+            for user in users.users {
+                println!("{}", user.user_name);
+            }
         }
-    },
-    Err(_) => todo!(),
+               
+            Err(err) => {
+                println!("Error occurred starting IAM client: {}", err);
+            }
+    }
 }
 
-}
+
+
 pub async fn send_dynamodb(config: Config) {
 // Call the function to create DynamoDB table
 let dynamoclient = dynamosdk::start_dynamodb().await;
