6c32477826399b50f862f92c61f5cdb18bb060d7
Ivan <jicxer@gmail.com>
2024-02-14 14:32:13 -0800

    Fixed conflicts

--- src/dynamosdk.rs
+++ src/dynamosdk.rs
@@ -1,39 +1,61 @@
-use crate::producer;
-use producer::start_log_stream;
-
-
 use aws_config::meta::region::RegionProviderChain;
 use aws_sdk_dynamodb::error::SdkError;
 use aws_sdk_dynamodb::primitives::Blob;
-use aws_sdk_dynamodb::{config::Region, meta::PKG_VERSION, Client, Error};
+use aws_sdk_dynamodb::{config::Region, meta::PKG_VERSION, Error};
 use aws_sdk_dynamodb::operation::create_table::{CreateTableOutput,CreateTableError};
-use crate::config::Config;
+use aws_sdk_dynamodb::operation::put_item::{PutItemOutput, PutItemError};
 use aws_sdk_dynamodb::error::{BuildError};
 
 use aws_sdk_dynamodb::types::{
     AttributeDefinition, KeySchemaElement, KeyType, ProvisionedThroughput, ScalarAttributeType,
 };
+use async_trait::async_trait;
+use std::sync::mpsc::{Receiver};
+use crate::dynamosdk;
+use crate::traits::DataHandler;
+use crate::config::Config;
+use crate::producer::start_log_stream;
+use aws_sdk_dynamodb::Client as DynamodbClient;
+use aws_sdk_dynamodb::types::AttributeValue;
 
-pub async fn
-start_dynamo() -> Result<Client, Error> {
-    let config = aws_config::load_from_env().await;
-    let client = aws_sdk_dynamodb::Client::new(&config);
-    Ok(client)
+#[async_trait]
+impl DataHandler for DynamodbClient {
+    fn show(&self) -> String {
+        format!("DynamodbClient: {:?}", &self)
+    }
+    fn clone_self(&self) -> Self {
+        self.clone()
+    }
+    async fn handle_log_data(&self, log_channel: Receiver<String>) {
+        if let Ok(table) = self.describe_table().table_name("eptesttable").send().await {
+            for log_line in log_channel {
+                println!("{log_line}");
+                let res = self.put_item()
+                    .table_name("eptesttable")
+                    .item("epkeyitem",AttributeValue::S(log_line))
+                    .send().await;
+                // dumb error checking for now. eventually, this will need to be 
+                // sent to the status api for the user.
+                //println!("{res:?}");
+            }
+        }
+    }
 }
+
 pub fn show_dynamodb_tables() -> Result<(), Error> {
     todo!();
     Ok(())
 }
 
 pub async fn
-start_dynamodb() -> Result<Client, Error> {
+create_client() -> Result<DynamodbClient, Error> {
     let config = aws_config::load_from_env().await;
     let client = aws_sdk_dynamodb::Client::new(&config);
     Ok(client)
 }
 
 pub async fn create_table(
-    client: &Client,
+    client: &DynamodbClient,
     table: &str,
     key: &str,
 ) -> Result<CreateTableOutput, String> {
@@ -77,8 +99,7 @@ pub async fn create_table(
 }
 
 pub async fn send_dynamodb(config: Config) {
-    // Call the function to create DynamoDB table
-    let dynamoclient = start_dynamodb().await;
+    let dynamoclient = dynamosdk::create_client().await;
     match dynamoclient {
         Ok(client) => {
             // check if the table listed in the configuration file
@@ -94,10 +115,10 @@ pub async fn send_dynamodb(config: Config) {
                     println!("found {tbl:?}");
                     // use the table
                     let _ = start_log_stream(config.sources.logs.clone(),
-                        &client).await;
+                                &client).await;
                 }
-            }
-            if let Ok(table) = create_table(&client,
+            } 
+            if let Ok(table) = dynamosdk::create_table(&client,
                                     "default_table",
                                     "default_key").await {
                 println!("{table:?}");
